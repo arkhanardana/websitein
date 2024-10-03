43dd7f3be46722d92daa30052a68065889f82854
arkhanardana <hanzmantap@gmail.com>
2024-10-03 21:19:56 +0700

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<link rel="icon" href="/assets/logo.jpeg" className="mix-blend-difference" />
 			<body className={font.className}>{children}</body>
 		</html>
 	);
